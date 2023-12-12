c9146b807841794c23421a7c717df382f75a1afa
Marika <marika.meinander@outlook.com>
2023-12-12 14:15:14 +0200

    Removed obsolete

--- pages/csr/create-post.tsx
+++ pages/csr/create-post.tsx
@@ -1,6 +1,6 @@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useAuth, useUser } from "@clerk/nextjs";
 import { useRouter } from 'next/navigation';
 import { LANGUAGES } from "@/lib/contants";
@@ -10,8 +10,7 @@ const CreatePostPage = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [language, setLanguage] = useState("");
-  const [authorId, setAuthorId] = useState("");
-  const { getToken, isLoaded, isSignedIn, userId } = useAuth();
+  const { userId } = useAuth();
   const { user } = useUser();
   const router = useRouter();
 
@@ -43,35 +42,6 @@ const CreatePostPage = () => {
       // Here could be a toast
     }
   }
-
-  //const handleSubmit = (event) => {
-    //event.preventDefault()
-    //savePost()
-  //}
-/*
-  const handleSubmit = async e => {
-    e.preventDefault();
-    //setError("");
-    //setMessage("");
-    if (title && content) {
-        // send a request to the server.
-        try {
-            const body = { title, content, published: false };
-            await fetch(`/api/post`, {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(body),
-            });
-            await Router.push("/drafts");
-        } catch (error) {
-            console.error(error);
-        }
-    } else {
-        //setError("All fields are required");
-        return;
-    }
-  }*/
-
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center px-0 py-10 sm:px-10 ">
